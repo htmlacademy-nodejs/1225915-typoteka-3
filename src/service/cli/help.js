1449107deaa58b26cc4349ede399ea3a193826d0
Anton Vavilov <vavilovonline@gmail.com>
2021-11-19 10:37:02 +0300

    1.16

---
+++ src/service/cli/help.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const version = require('./version');
+const generate = require('./generate');
+
+module.exports = {
+  name: '--help',
+  run() {
+    const text = `
+    Программа запускает http-сервер и формирует файл с данными для API.
+
+        Гайд:
+        service.js <command>
+        Команды:
+         ${version.name}: выводит номер версии
+         ${this.name}: печатает этот текст
+         ${generate.name} <count> формирует файл mocks.json
+    `;
+
+    console.info(text);
+  },
+};
