dd4ae81ac760ffa63ac95f355ecff1f8ae9a4761
Anton Vavilov <vavilovonline@gmail.com>
2021-11-19 10:44:44 +0300

    corrected constants.

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 module.exports.DEFAULT_COMMAND = `--help`;
 
@@ -6,7 +6,7 @@ module.exports.USER_ARGV_INDEX = 2;
 
 module.exports.ExitCode = {
   error: 1,
-  success: 0
+  success: 0,
 };
 
-module.exports.MOCK_FILE_NAME = `mocks.json`;
+module.exports.MOCK_FILE_NAME = `mock.json`;
