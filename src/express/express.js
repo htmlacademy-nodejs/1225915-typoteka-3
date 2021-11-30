5e1ad47d7055212e1e93aa6f2bc845e3bac9dc53
Anton Vavilov <vavilovonline@gmail.com>
2021-11-30 16:29:59 +0300

    3.17

--- src/express/express.js
+++ src/express/express.js
@@ -11,12 +11,12 @@ const { HttpCode } = require('../constants');
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
-const TEMPLATES_DIR = `templates/pages`;
+const TEMPLATES_PATH = path.join('templates', 'pages');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
-app.set('views', path.resolve(__dirname, TEMPLATES_DIR));
+app.set('views', path.resolve(__dirname, TEMPLATES_PATH));
 app.set('view engine', 'pug');
 
 app.use(BASE_MAIN_PATH, mainRouter);
